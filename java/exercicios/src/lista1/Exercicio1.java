050010e6d5ba4daa12ea0115347c35cc621b5fb3
Ricardo Martins <martinsc.ricardo@gmail.com>
2020-10-07 15:38:35 -0300

    alterações lista 1 exercicio 1 de java

--- java/exercicios/src/lista1/Exercicio1.java
+++ java/exercicios/src/lista1/Exercicio1.java
@@ -22,11 +22,7 @@ public class Exercicio1 {
 				System.out.println("O maior número é " + num3);
 			}
 		} else if (num2 > num3) {
-			if (num2 > num3) {
-				System.out.println("O maior número é " + num2);
-			} else {
-				System.out.println("O maior número é " + num3);
-			}
+			System.out.println("O maior número é " + num2);
 		} else {
 			System.out.println("O maior número é " + num3);
 		}
