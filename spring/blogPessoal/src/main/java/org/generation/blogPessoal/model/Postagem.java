c2e3fbd5131358661d388040b9e73beaccc7c137
Ricardo Martins <martinsc.ricardo@gmail.com>
2020-11-06 09:44:47 -0300

    blog pessoal relacionamento de tabelas

--- spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 //import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -15,6 +16,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_postagens")
@@ -37,6 +40,10 @@ public class Postagem {
 	@CreationTimestamp
 	private Date data;
 	// = new java.sql.Date(System.currentTimeMillis())
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
 	
 	public long getId() {
 		return id;
@@ -68,6 +75,14 @@ public class Postagem {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 }
