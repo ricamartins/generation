a5f7cbd420d3503f677d71d9d563b1d8d3209983
Ricardo Martins <martinsc.ricardo@gmail.com>
2020-11-25 20:14:42 -0300

    bootstrap time

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -10,15 +10,17 @@ function validaNome() {
     let txt = document.querySelector('#txtNome')
     
     if (nome.value == '') {
-        txt.innerHTML = ''
+        txt.style.display = 'none'
         nomeOK = false
     } else if (nome.value.length < 3) {
         txt.innerHTML = 'Nome inválido'
-        txt.style.color = 'red'
+        txt.classList.add('alert-success')
+        txt.classList.add('alert-danger')
         nomeOK = false
     } else {
         txt.innerHTML = 'Nome válido'
-        txt.style.color = 'green'
+        txt.classList.remove('alert-danger')
+        txt.classList.add('alert-success')
         nomeOK = true
     }
 }
@@ -27,15 +29,17 @@ function validaEmail() {
     let txt = document.querySelector('#txtEmail')
     
     if (email.value == '') {
-        txt.innerHTML = ''
+        txt.style.display = 'none'
         emailOK = false
     } else if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
         txt.innerHTML = 'Email inválido'
-        txt.style.color = 'red'
+        txt.classList.remove('alert-success')
+        txt.classList.add('alert-danger')
         emailOK = false
     } else {
         txt.innerHTML = 'Email válido'
-        txt.style.color = 'green'
+        txt.classList.remove('alert-danger')
+        txt.classList.add('alert-success')
         emailOK = true
     }
 }
@@ -43,15 +47,16 @@ function validaEmail() {
 function validaAssunto() {
     let txt = document.querySelector('#txtAssunto')
     
-    if (assunto.value.length == '') {
-        txt.innerHTML = ''
+    if (assunto.value == '') {
+        txt.style.display = 'none'
         assuntoOK = false
     } else if (assunto.value.length > 100) {
         txt.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
-        txt.style.color = 'red'
+        txt.style.display = 'block'
+        txt.classList.add('alert-danger')
         assuntoOK = false
     } else {
-        txt.innerHTML = ''
+        txt.style.display = 'none'
         assuntoOk = true
     }
 }
@@ -65,8 +70,8 @@ function enviar() {
 }
 
 function mapaZoom() {
-    mapa.style.width = '600px'
-    mapa.style.height = '600px'
+    mapa.style.width = '450px'
+    mapa.style.height = '450px'
 }
 
 function mapaNormal() {
